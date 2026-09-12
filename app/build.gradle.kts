8c8d0d71194b7ef01c09f0707ceffecde147d2ae
belowevolve <130287859+belowevolve@users.noreply.github.com>
2026-09-13 01:36:58 +0300

    chore(gradle): remove unused deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.protobuf.javalite)
     testImplementation(libs.junit)
 }
